9fd36c1f0c3bdb35456294d2bfb6cce347b04d06
Cole Speers <ccspeers@ucdavis.edu>
2020-02-12 12:46:48 -0800

    Update to initial cURL

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 from random import randint
 
 app = Flask(__name__)
@@ -7,11 +7,13 @@ cashe = {'favorite number': None}
 
 @app.route('/')
 def index():
-    return 'Interact with data using /read and /update'
+    return 'Interact with data using /read, /update, or /favorite_number'
+
 
 @app.route('/read')
 def read():
     return str(cashe)
+
 
 @app.route('/update')
 def update():
@@ -19,5 +21,20 @@ def update():
     return str(cashe)
 
 
+@app.route('/favorite_number', methods=['GET','POST'])
+def fav_num():
+    print(f'My json was {request.json}')
+    print(f'My form was {request.form}')
+    print(f'My args was {request.args}')
+    print(f'My method was {request.method}')
+    return ''
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+# request.args.get('key')
+#$ curl -d "{\"key1\":\"value1\", \"key2\":\"value2\"}" -H "Content-Type: application/json" -X GET http://127.0.0.1:5000/favorite_number?foo=bar
+#$ curl -d "{\"key1\":\"value1\", \"key2\":\"value2\"}" -H "Content-Type: application/json" -X POST http://127.0.0.1:5000/favorite_number?foo=bar
+#$ curl -d "{\"key1\":\"value1\", \"key2\":\"value2\"}" -X POST http://127.0.0.1:5000/favorite_number?foo=bar
+#$ curl -d "{\"key1\":\"value1\", \"key2\":\"value2\"}" -X GET http://127.0.0.1:5000/favorite_number?foo=bar
