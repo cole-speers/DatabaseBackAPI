7007178681142931f8fce329a74dabfc1f96d198
Cole Speers <ccspeers@ucdavis.edu>
2020-02-13 10:49:45 -0800

    Consolidate /read /update into favorite_number

--- main.py
+++ main.py
@@ -4,25 +4,14 @@ from random import randint
 app = Flask(__name__)
 cache = {'favorite number': None}
 
-
+# Landing Page
 @app.route('/')
 def index():
-    return 'Interact with data using /read, /update, or /favorite_number'
+    return 'Interact with data using /favorite_number or /debug'
 
-
-@app.route('/read')
-def read():
-    return f'read favorite number {str(cache)}'
-
-
-@app.route('/update')
-def update():
-    cache["favorite number"] = randint(1, 100)
-    return f'updated favorite number {str(cache)}'
-
-
-@app.route('/favorite_number', methods=['GET','POST'])
-def fav_num():
+# Background Info to Show on Terminal
+@app.route('/debug', methods=['GET','POST'])
+def debug():
     print(f'My json was {request.json}')
     print(f'My form was {request.form}')
     print(f'My args was {request.args}')
@@ -30,19 +19,27 @@ def fav_num():
     return ''
 
 
-# Creating new route that updates cashe with either GET or POST.
-@app.route('/activate', methods=['GET','POST'])
-def activate():
+# Updates cashe with either GET or POST.
+@app.route('/favorite_number', methods=['GET','POST'])
+def favorite_number():
     if request.method == 'GET':
-        return read()
+        return f'read favorite number {str(cache)}'
     elif request.method == 'POST':
-        return update()
+        cache["favorite number"] = randint(1, 100)
+        return f'updated favorite number {str(cache)}'
 
 if __name__ == '__main__':
     app.run(debug=True)
 
-# request.args.get('key')
-#$ curl -d "{\"key1\":\"value1\", \"key2\":\"value2\"}" -H "Content-Type: application/json" -X GET http://127.0.0.1:5000/favorite_number?foo=bar
-#$ curl -d "{\"key1\":\"value1\", \"key2\":\"value2\"}" -H "Content-Type: application/json" -X POST http://127.0.0.1:5000/favorite_number?foo=bar
-#$ curl -d "{\"key1\":\"value1\", \"key2\":\"value2\"}" -X POST http://127.0.0.1:5000/favorite_number?foo=bar
-#$ curl -d "{\"key1\":\"value1\", \"key2\":\"value2\"}" -X GET http://127.0.0.1:5000/favorite_number?foo=bar
+
+'''
+Terminal Commands:
+
+$ curl -d "{\"key1\":\"value1\", \"key2\":\"value2\"}" -H "Content-Type: application/json" -X GET http://127.0.0.1:5000/favorite_number?foo=bar
+$ curl -d "{\"key1\":\"value1\", \"key2\":\"value2\"}" -H "Content-Type: application/json" -X POST http://127.0.0.1:5000/favorite_number?foo=bar
+$ curl -d "{\"key1\":\"value1\", \"key2\":\"value2\"}" -X POST http://127.0.0.1:5000/favorite_number?foo=bar
+$ curl -d "{\"key1\":\"value1\", \"key2\":\"value2\"}" -X GET http://127.0.0.1:5000/favorite_number?foo=bar
+
+'''
+
+# We'll need this later:     request.args.get('key')
